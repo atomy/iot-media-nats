ba0413c7bd667cf1b5ee322f2875d6a0ed85fe43
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-08-19 06:05:05 +0200

    - super duper pretty format - elelelele

--- scripts/notification.sh
+++ scripts/notification.sh
@@ -25,7 +25,7 @@ then
     -d "{\"username\": \"Jenkins-Release\", \"content\": \"Released **${APP_NAME}** -- **${CURRENT_VERSION}** -> **${NEW_VERSION}**\nunable to determine changes\"}" \
     ${DISCORD_WEBHOOK_URL}
 else
-  CHANGES=`git log ${CURRENT_VERSION}..${NEW_VERSION} | sort | uniq`
+  CHANGES=`git log --pretty=format:%B ${CURRENT_VERSION}..${NEW_VERSION} | sort | uniq`
   CHANGES=`echo ${CHANGES} | sed ':a;N;$!ba;s/\n/\\n/g'`
 
   curl -X POST \
