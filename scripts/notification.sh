657b838df5f5678552a9d47394cf2c2d5dd706c9
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-08-19 06:01:28 +0200

    - fixin notification script - i am a multi-\nline comment

--- scripts/notification.sh
+++ scripts/notification.sh
@@ -26,7 +26,7 @@ then
     ${DISCORD_WEBHOOK_URL}
 else
   CHANGES=`git log ${CURRENT_VERSION}..${NEW_VERSION} | sort | uniq`
-  CHANGES=`echo ${CHANGES} | sed 's/\n/\n/'`
+  CHANGES=`echo ${CHANGES} | sed ':a;N;$!ba;s/\n/\\n/g'`
 
   curl -X POST \
     -H "Content-Type: application/json" \
