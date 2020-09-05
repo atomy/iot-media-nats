9440c8ac70fd97fbb1eefb912a56f1ec84e6fb7b
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-09-05 19:04:13 +0200

    init

---
+++ scripts/notification.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+NEW_VERSION=`git describe --abbrev=0 --tags`
+
+if [ -z "${DISCORD_WEBHOOK_URL}" ] ; then
+  echo "ENV: DISCORD_WEBHOOK_URL is missing!"
+  exit 1
+fi
+
+if [ -z "${APP_NAME}" ] ; then
+  echo "ENV: APP_NAME is missing!"
+  exit 1
+fi
+
+CURRENT_VERSION=`cat current_live_version`
+
+if [[ "" == "${CURRENT_VERSION}" ]]
+then
+  CURRENT_VERSION="?"
+
+  curl -X POST \
+    -H "Content-Type: application/json" \
+    -d "{\"username\": \"Jenkins-Release\", \"content\": \"Released **${APP_NAME}** -- **${CURRENT_VERSION}** -> **${NEW_VERSION}**\nunable to determine changes\"}" \
+    ${DISCORD_WEBHOOK_URL}
+else
+  CHANGES=`git log --pretty=format:%B ${CURRENT_VERSION}..${NEW_VERSION} | sort | uniq`
+  CHANGES=`echo ${CHANGES} | sed ':a;N;$!ba;s/\n/\\\n/g'`
+
+  curl -X POST \
+    -H "Content-Type: application/json" \
+    -d "{\"username\": \"Jenkins-Release\", \"content\": \"Released **${APP_NAME}** -- **${CURRENT_VERSION}** -> **${NEW_VERSION}**\n${CHANGES}\"}" \
+    ${DISCORD_WEBHOOK_URL}
+fi
