139dbc239f5b1e33b5333e2e8944cc581841c53d
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-09-06 04:39:29 +0200

    - init

--- scripts/auto-tag.sh
+++ scripts/auto-tag.sh
@@ -21,14 +21,14 @@ echo "[auto-tag] Increasing \"$VERSION\" to \"$NEW_TAG\""
 
 # get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null || true`
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
-    git tag $NEW_TAG
+    echo git tag $NEW_TAG
     echo "[auto-tag] Tagged with $NEW_TAG"
     git remote set-url origin git@github.com:atomy/iot-media-nats.git
-    git push --tags
+    echo git push --tags
 else
     echo "[auto-tag] Already a tag on this commit"
 fi
